e0f8fa99077fcd7491f315ae500e827b313da4fa
yuki-k112 <60835872+yuki-k112@users.noreply.github.com>
2020-03-20 02:08:49 +0900

    category validate修正

--- pages/_category.vue
+++ pages/_category.vue
@@ -5,11 +5,9 @@
 
 <script>
 export default {
-    async validate ({ params, store }) {
-        const categories = await store.getters['contents/getCategories'].map(item => {
-            return item.toLowerCase().replace( /\// , "-" )
-        })
+    validate ({ params, store }) {
+        const categories = store.getters['contents/getCategories'].map(item => item.path)
         return categories.some(category => category === params.category);
-  }
-}
+    }
+}       
 </script>
